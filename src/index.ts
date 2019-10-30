4ae93345ad66508539499563291ebccf283a79c0
liyang <leeyang@egret.com>
2019-10-30 19:16:42 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,9 @@
-import * as fs from 'fs-extra';
 import * as path from 'path';
-import walk from 'walk-sync';
-import stripJsonComments from 'strip-json-comments';
 import {execSync,exec} from "child_process";
-import convert from "xml-js";
-const gamesPath = `C:/Users/L-/Desktop/reverse/lib/games/`;
-const libPath = `C:/Users/L-/Desktop/reverse/lib/`;
+
 //当前工作路径
-var cwd:string;
-function CMD(cmd:string) {
+export var cwd:string;
+export function CMD(cmd:string) {
     if (cmd.includes('cd ')) {
         cwd = cmd.replace('cd ', '');
         return;
@@ -26,57 +21,25 @@ function CMD(cmd:string) {
         console.log(cmd + '---------------------finish');
 
 }
-
-function run(){
-    const sourceApkPath = libPath+"app-release.apk";
-    const reverseOutPath = libPath+"out";
-    
-//拆包
-    // if(fs.existsSync(sourceApkPath)){
-    //     console.log(111);
-    //     fs.ensureDirSync(reverseOutPath);
-    //     const commands = [
-    //         `cd ${libPath}`,
-    //         `rm -rf ${reverseOutPath}`,
-    //         `mkdir out`,
-    //         `apktool.bat d -f app-release.apk -o out` 
-    //     ];
-    //     for (const c of commands) {
-    //         CMD(c);
-    //     }
-    // }
-    //games
-    const data:{[key:string]:string} = {
-        animalchess:"斗兽棋",
-        BANG:"BANG"
-    };
-        
-    //转移资源
-
-    for (const key in data) {
-        if (data.hasOwnProperty(key)) {
-            const element = data[key];
-            editFiles({gamename:element,id:key});
-        }
+export function CMDS(cmds:string[]){
+    for (const cmd of cmds) {
+        CMD(cmd);
     }
 }
-function editFiles(arg:any){
-    const gamename = arg.gamename;
-    const id = arg.id;
+export function unpack(apk:string,out:string){
+    const libpath = path.join(__dirname,"../lib");
     const commands = [
-        `cd ${libPath}`,
-        `rm -rf out/assets/game`,
-        `cp -r games/${id} out/assets/game`,
-        `mv out/assets/game/icon.png out/res/drawable/icon.png` 
+        `cd ${libpath}`,
+        `apktool.bat d -f ${apk} -o ${out}` 
     ];
-    for (const c of commands) {
-        CMD(c);
-    }
-    const xmlStrings = fs.readFileSync(libPath+'out/res/values/strings.xml',"utf-8");
-    const objStrings = convert.xml2js(xmlStrings,{compact:true}) as any;
-    objStrings.resources.string._text = gamename;
-    const result = convert.js2xml(objStrings,{compact:true});
-    
-
+    CMDS(commands);
 }
-run();
+export function packandsign(src:string,out:string,keystore:{file:string,alias:string,storepass:string,keypass:string}){
+    const libpath = path.join(__dirname,"../lib");
+    const commands = [
+        `cd ${libpath}`,
+        `apktool.bat b ${src} -o unsigned.apk`,
+        `jarsigner -verbose -keystore ${keystore.file} -signedjar ${out} unsigned.apk  key0 -storepass "${keystore.storepass}" -keypass "${keystore.keypass}"`
+    ];
+    CMDS(commands);
+}
